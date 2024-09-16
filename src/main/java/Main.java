a7258b65da749b324d58641c581c97d130e7d09e
Alexander Sergeev <a79164220703@gmail.com>
2024-09-16 18:28:08 +0300

    add Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import maze.interaction.Interaction;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Interaction action = new Interaction();
+        action.printMenu();
+    }
+}
