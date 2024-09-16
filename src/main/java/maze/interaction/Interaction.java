bac35a13f283e4e084e3ca7638df4e37dea955bb
Alexander Sergeev <a79164220703@gmail.com>
2024-09-16 18:34:05 +0300

    add Interaction class

---
+++ src/main/java/maze/interaction/Interaction.java
@@ -0,0 +1,14 @@
+package maze.interaction;
+
+import java.util.Scanner;
+
+public class Interaction {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public void printMenu() {
+        while (true) {
+            System.out.println();
+        }
+    }
+}
