9bd48c3f52118571fa0e45fd69b8b33bb278c047
Alexander Sergeev <a79164220703@gmail.com>
2024-09-16 20:24:37 +0300

    finished Point class

--- src/main/java/maze/model/Point.java
+++ src/main/java/maze/model/Point.java
@@ -1,30 +1,6 @@
 package maze.model;
 
-public class Point {
-
-    private int x;
-    private int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
+public record Point(int x, int y) {
 
     @Override
     public String toString() {
